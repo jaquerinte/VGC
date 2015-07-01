4073b0516ba672ce27f0002472e8be766c20e3b3
Jaquer <ivanrodriguezferrandez@gmail.com>
2015-07-01 13:20:33 +0200

    añnadido en layout de invitados y cla clase encargada de modificarlos

---
+++ app/src/main/java/com/jsadevtech/jsa/vgc/invitados_screen.java
@@ -0,0 +1,45 @@
+package com.jsadevtech.jsa.vgc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by Jaquer on 01/07/2015.
+ *
+ * clase encargada de manejar la pantalla de invitados
+ */
+public class invitados_screen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invitados_screen);
+        StrictMode.enableDefaults();//modo stricto necesario para la conexion a internet
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_screen, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
