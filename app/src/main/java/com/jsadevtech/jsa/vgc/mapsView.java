a665cd6213570d687edff3ecf5c27c7aadaefbb9
jaquer <ivanrodriguezferrandez@gmail.com>
2015-07-04 12:43:49 +0200

    añadido modificacion al mapa para marcar el sitio

--- app/src/main/java/com/jsadevtech/jsa/vgc/mapsView.java
+++ app/src/main/java/com/jsadevtech/jsa/vgc/mapsView.java
@@ -2,11 +2,15 @@ package com.jsadevtech.jsa.vgc;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class mapsView extends FragmentActivity implements OnMapReadyCallback {
@@ -25,12 +29,43 @@ public class mapsView extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap map) {
         // pone un marcador en ifa y mueve la camara
-        LatLng ifa = new LatLng(38.296248,-0.575816);
+        LatLng ifaPosition = new LatLng(38.296248,-0.575816);
         map.setMyLocationEnabled(true);
-        map.addMarker(new MarkerOptions().position(ifa).title("IFA, donde se celebra la VGC"));
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(ifa,10));
+        Marker ifa=map.addMarker(new MarkerOptions()
+                        .position(ifaPosition)
+                        .title("VGC 2015")
+                        .snippet("Como te lo vas a perder")
+
+        );
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(ifaPosition,10));
+        ifa.showInfoWindow();
+
         //map.moveCamera(CameraUpdateFactory.newLatLng(ifa));
 
 
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_screen, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
